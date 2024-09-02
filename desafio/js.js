3bdb352b61af5afd65f433deb0ceb4f954cc37c4
Athos Montarroyos <athosg05@gmail.com>
2024-09-02 14:52:37 -0300

    terminei

--- desafio/js.js
+++ desafio/js.js
@@ -1,24 +1,27 @@
 var primeiro = 0
-var final = 100
+var final = prompt('Ate onde eu iremos?')
 console.log(`${final}`)
 if (final <= 0) {
     alert('[ERROR]')
 } else if (final == null){
     alert('[ERROR]') }
-    tabela = ''
-while(tabela < final) {
     var numeroanterior = 0
     var numeroproxterior = 1
-    var tabela = numeroanterior + numeroproxterior
-    console.log(`${tabela}`)
+    var tabela = 0
+while(tabela < final) {
+
+   
+    
+
+    var somadosnumb = numeroanterior + numeroproxterior
+    //1
     let numeroanterior1 = numeroproxterior
-    console.log(`${numeroanterior1}`)
-    let numeroproxterior1 = tabela
-    console.log(`${numeroproxterior1}`)
+    //1
+    let numeroproxterior1 = somadosnumb
+    //1
     var numeroproxterior = numeroproxterior1
-    console.log(`${numeroproxterior}`)
     var numeroanterior = numeroanterior1
-    console.log(`${numeroanterior}`)
-    console.log(`${tabela}`)
-} 
+    var tabela = somadosnumb
+    alert(`${tabela}`)
+}
 
