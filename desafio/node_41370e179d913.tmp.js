43ff02491f8ee40d8a6977e47eede3fe58197484
Athos Montarroyos <athosg05@gmail.com>
2024-09-02 15:59:25 -0300

    aaaaaaaa

--- desafio/node_41370e179d913.tmp.js
+++ desafio/node_41370e179d913.tmp.js
@@ -12,8 +12,9 @@ do {
     var numero1 = ''
     var numero2 = ''
     var soma = numero1 + numero2
+    tabela = soma
     var numero1 = anterior
-    var numero2 = soma
-    var tabela = numero2
+    var numero2 = tabela
+    var tabela = soma
     console.log(`${tabela}`)
    }  while (tabela < final)
