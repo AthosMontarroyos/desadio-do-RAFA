2eb9878b58fc29623c803b1249aba8c0c52268e7
Athos Montarroyos <athosg05@gmail.com>
2024-09-02 13:55:56 -0300

    desafio

---
+++ desafio/js.js
@@ -0,0 +1,16 @@
+var primeiro = 0
+var final = prompt('Ate onde vc quer chegar?')
+console.log(`${final}`)
+let numero1 = 0
+let numero2 = 1
+var tabela = numero1 + numero2
+if (final <= 0) {
+    alert('[ERROR]')
+} else if (final == null){
+    alert('[ERROR]')
+}    else {
+while(tabela < final) {
+    var numeroanterior = 0
+    var numeroproxterior = 1
+} 
+}
